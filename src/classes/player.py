42c4468a7e021c1887e780c540c3ec12c69dbb00
spanifer <kovacsszabolcs91@gmail.com>
2022-01-12 01:09:41 +0000

    Add player class, and ask player for a name

---
+++ src/classes/player.py
@@ -0,0 +1,16 @@
+class Player:
+    '''
+    Creates an instance for the human or the computer player
+    '''
+    def __init__(self, name):
+        self.__name = name
+    
+    __shot_count = 0
+
+    @property
+    def name(self):
+        return self.__name
+
+    def add_shot():
+        '''Increment the shot counter by one'''
+        self.__shot_count += 1
--- src/game.py
+++ src/game.py
@@ -1,9 +1,9 @@
 import os
 from src.classes.ships import *
-
+from src.classes.player import Player
 
 # The defined game rule requires the following number of ships
-no_of_ships = [
+list_of_ships = [
     [1, AircraftCarrier()],
     [1, Battleship()],
     [1, Cruiser()],
@@ -11,10 +11,41 @@ no_of_ships = [
     [2, Submarine()]
 ]
 
+
+def choose_name():
+    print('\nChoose a name for your captain (maximum 16 character)')
+    name = input('Name: ')
+
+    if len(name) > 16:
+        name = name[:16]
+
+        def agree_on_name():
+            print(f'Your name will be: {name}')
+            agreement = input('Is this ok? (y)es/(n)o ').lower()
+            if agreement in ('n','no'):
+                return choose_name()
+            elif agreement in ('y','yes'):
+                return name
+            else:
+                print(f'Invalid answer:\n{agreement}')
+                return agree_on_name()
+        
+        name = agree_on_name()
+
+    return name
+
+
+def chose_placement_type():
+    '''
+    Allows the user to chose from random or manual ship placement
+    '''
+    print('Do you want to place your ships on the grid or want it randomly placed?')
+    print('Type (R)andom or (M)anual: ')
+
+
 def start_game():
     '''Game entry point'''
     os.system('cls||clear')
 
-    print('Game')
-    for ships in no_of_ships:
-        print(ships[0], ships[1].name)
+    player = Player(choose_name())
+    placement = chose_placement_type()
