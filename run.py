7348feee7ef1456712325ac96a6a1b45be53953f
spanifer <kovacsszabolcs91@gmail.com>
2022-01-17 10:36:11 +0000

    Add last resort test

--- run.py
+++ run.py
@@ -5,7 +5,13 @@ def main():
     '''
     Application entry point, show interactive menu
     '''
-    menu()
+    try:
+        menu()
+    except KeyboardInterrupt:
+        print(' Good bye...')
+    except Exception as err:  # Gracefully exit the app
+        err  # Could save it to a file
+        input('Something went wrong. 😥')
 
 
 main()
