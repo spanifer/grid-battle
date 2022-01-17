89f3db9d4bbe2f3978d4c60dfbcbbc24ec60e978
spanifer <kovacsszabolcs91@gmail.com>
2022-01-17 01:22:03 +0000

    Add API handler methods for high_scores

--- src/high_score.py
+++ src/high_score.py
@@ -8,7 +8,38 @@ SCOPE = [
     ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scope(SCOPE)
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('grid_battle')
 high_scores = SHEET.worksheet('high_scores')
+
+
+def get_scores(msg='Fetching High Scores...'):
+    '''Prints an optional message before sending the request
+    Returns scores list with header sliced'''
+    print(msg)
+    return high_scores.get_all_values()[1:]
+
+
+def is_high_score(scores, new_score):
+    '''Checks if new_score is in top 10 by compareing it to
+    the last element of the concatenated and sorted list'''
+    if len(scores) < 10:
+        return True
+    if new_score is not sort_scores(scores, new_score).pop():
+        return True
+    return False
+
+
+def sort_scores(scores, new_score):
+    '''Append new score to scores
+    Sorts the list by shot count value'''
+    scores.append(new_score)
+    return scores.sort(key=lambda r: int(r[1]))
+
+
+def add_to_scores(score, index):
+    '''Insert row to index
+    NOTE: This somehow runs in sync'''
+    high_scores.delete_rows(11)
+    high_scores.insert_row(score, index+1)
